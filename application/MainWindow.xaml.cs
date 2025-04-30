eec9ee901b0ba28dd79f6cefa82ae330141e8e78
esc <e.schuckmann@pm.me>
2025-05-01 01:17:34 +0200

    [TASK] Add initial WPF project setup
    Initialize a new WPF application with .NET 9.0, including App.xaml, MainWindow.xaml, and supporting files like AssemblyInfo.cs and project file. This serves as the foundation for building the application's user interface and functionality.

---
+++ application/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace application;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
