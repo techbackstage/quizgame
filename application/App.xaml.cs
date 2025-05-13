fa3c2f5e44a92543173d970d684af686aa5275ee
techbackstage <e.schuckmann@proton.me>
2025-05-13 11:38:33 +0200

    YEAAH

--- application/App.xaml.cs
+++ application/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// filepath: c:\Users\Erik\RiderProjects\quizgame\application\App.xaml.cs
+using System;
 using System.Windows;
 using System.IO;
 
@@ -9,31 +10,21 @@ namespace application
     /// </summary>
     public partial class App : Application
     {
-        // Add unhandled exception handler
         public App()
         {
-            // Log to file as the console output might not be visible
-            File.AppendAllText("app_log.txt", $"{DateTime.Now}: App constructor called\n");
-            
+            // Set up app-level exception handling
             this.DispatcherUnhandledException += App_DispatcherUnhandledException;
-            this.Startup += App_Startup;
-            this.Exit += App_Exit;
-        }
-
-        private void App_Startup(object sender, StartupEventArgs e)
-        {
-            File.AppendAllText("app_log.txt", $"{DateTime.Now}: App started\n");
-        }
-
-        private void App_Exit(object sender, ExitEventArgs e)
-        {
-            File.AppendAllText("app_log.txt", $"{DateTime.Now}: App exited with code: {e.ApplicationExitCode}\n");
         }
 
         private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
-            File.AppendAllText("app_log.txt", $"{DateTime.Now}: UNHANDLED EXCEPTION: {e.Exception.Message}\n{e.Exception.StackTrace}\n");
-            MessageBox.Show("An unhandled exception occurred: " + e.Exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            // Log the exception
+            File.AppendAllText("error_log.txt", $"{DateTime.Now}: UNHANDLED EXCEPTION: {e.Exception.Message}\n{e.Exception.StackTrace}\n");
+            
+            // Show error message to user
+            MessageBox.Show($"An error occurred: {e.Exception.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            
+            // Mark as handled to prevent app crash
             e.Handled = true;
         }
     }
